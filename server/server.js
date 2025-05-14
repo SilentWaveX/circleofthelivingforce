cc42a9e1b9dbf311b34a136b752f66ff84177941
SilentWaveX <melisentaprincess@gmail.com>
2025-05-14 19:02:32 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ app.get('/', (req, res) => {
   res.send('Backend is working!');
 });
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => {
+  console.error('MongoDB connection error:', error.message);
+  process.exit(1);
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
