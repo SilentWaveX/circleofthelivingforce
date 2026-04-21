84873fe4df702ce0b83137f6a898fce98c0857f3
SilentWaveX <melisentaprincess@gmail.com>
2026-04-21 13:39:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const langSwitch = document.querySelector(".language-switch");
 
+    if (langSwitch) {
+        langSwitch.addEventListener("click", function () {
+            alert("Language switch works");
+        });
+    }
+});
